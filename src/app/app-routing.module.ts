87caa1618e8b4d6933926e53fd07d1e641e569d6
tecnicopa87 <52227751+tecnicopa87@users.noreply.github.com>
2020-07-09 12:12:19 -0500

    Correción de rutas menu principal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {RegisterComponent} from './security/register/register.component';
 import { AltaclientesComponent } from './altaclientes/altaclientes.component';
+import { ListaPlanesComponent } from './lista-planes/lista-planes.component';
 
 /* Creamos directiva para enrutamiento, apuntando a modulo security*/
 const routes: Routes = [
   {path:'clientes',component:AltaclientesComponent},  
+  {path:'listaplanes',component:ListaPlanesComponent},  
   {path:'**',pathMatch:'full',redirectTo:'clientes'},
   {
     path: 'register',
